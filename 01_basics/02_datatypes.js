fffeebdb4d72612dedabacf12a90f0196a93d774
Rakholiya jay <119782456+rakholiyajay@users.noreply.github.com>
2024-01-10 10:07:22 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+//Datatypes
+
+//Primitive Datatypes
+let name = "Jay"//string datatypes
+let age = 29 //number datatypes
+let isLoggedIn = true/false //boolean datatypes
+let undefined 
+//bigint = when we pas or print the greater value above the limit of the number that time  we use bigint
+//null => standalone value
+//undefined => when we not assign the value of any variable that time the undefined will be seen 
+//symbol => unique
+
+
+//NonPrimitive Datatypes
+//object
+
+// check types 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)//output object
+console.log(typeof undefined)
