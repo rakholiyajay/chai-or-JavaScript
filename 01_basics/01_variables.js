540ccadd9e6b9b3ec2fc1257b996202f9375f278
Rakholiya jay <119782456+rakholiyajay@users.noreply.github.com>
2024-01-10 08:05:32 +0000

    01 var_let_const describe

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 8849
+let accountEmail = "rakholiyajay147@gmail.com"
+var accountPassword = "Admin@123"
+accountCity = "Ahmedabad"
+let accountState;
+
+//try to change all the thing and test it 
+//accountId = 8980(it doesn't work because we uuse the constant the value of constant it ddoesn't change)
+accountEmail = "jay@gmail.com"
+accountPassword= "Admin"
+accountCity = "surat"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
