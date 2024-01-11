09e2b46d7463c0c7b1276364bfb4066cdd31134e
Rakholiya jay <119782456+rakholiyajay@users.noreply.github.com>
2024-01-11 08:44:39 +0000

    memory

--- 01_basics/datatypes_summery.js
+++ 01_basics/datatypes_summery.js
@@ -36,4 +36,30 @@ let myObj = {
 }//this is the example of the object the values is in {} is the Obj and the in the object the datatypes we can use any string, obj, fun also 
 const myFun = function() {
     console.log("Rakholiya Jay")
+}//this is the example of the function in the nonprimitive datatypes
+
+
+//*****************************************************
+//2 types of the memory
+//1) Stack(Primitive)
+//2) Heap(Non-Primitive)
+
+//:- The stack memory is used in the all of the Primitive datatypes & Heap memory is used in the Non-Primitive datatypes
+
+//stack:- when we use stack memory that time the it gives the copy of that value we should change on the copy
+// example:-
+ let myname = "Jay"
+ let fullname = myname
+ fullname = "Rakholiya Jay"
+ console.log(myname) 
+ console.log(fullname) 
+//Heap:- when we use the Heap memory that time it gives a Reference that why it automatically change on the mainvalue
+let userOne = {
+    email: "jay@gmail.com",
+    upi: "jay@ybl"
 }
+let userTwo = userOne
+userTwo.email = "rakholiyajay147@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
