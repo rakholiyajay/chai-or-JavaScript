6983b069ed0028d755d5a8966b5c8247c74e774b
Rakholiya jay <119782456+rakholiyajay@users.noreply.github.com>
2024-01-16 07:30:05 +0000

    function basic part 1   > object in function   > Array in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -21,7 +21,7 @@ function addTwoNum(number1, number2){
 }
 
 const result = addTwoNum(6,5)
-console.log("result :",result)
+//console.log("result :",result)
 
 
 
@@ -33,7 +33,7 @@ function loginUserMessage(username){
   //console.log(loginUserMessage("Jay"))
 
  const loginMessage = loginUserMessage("Jay")
- console.log("loginMessage:",loginMessage)
+ //console.log("loginMessage:",loginMessage)
 
  //in some case any person didn't pass the value 
 
@@ -45,7 +45,7 @@ function loginUserMessage(username){
     return `${username2} Just Logged In`
 }
 
- console.log(loginUserMessage2())
+ //console.log(loginUserMessage2())
 
 
 //  gives the default value when any person not gives a username
@@ -54,4 +54,47 @@ function loginUserMessage3(username3 = "RakholiyaJay"){
     return `${username3} Just Logged In`//we doesn't use the if statement because the username value not undefined any time because we gives a default value 
 }
 
-console.log(loginUserMessage3())
+//console.log(loginUserMessage3())
+ 
+
+//this is the task that youdidn't know how much product will be add
+
+//rest operator is just like spread operator
+function calculateCartPrice(...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200, 300, 500, 2000))
+
+// 2nd example
+function calculateCartPrice2(value1, value2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice2(200, 300, 500, 2000))//output is [500, 2000] because ithe 200 is taken by value1 and 300 is taken by value 2
+
+
+//object pass in the function 
+const user = {
+    name: "Jay",
+    price: 199
+}
+
+function handleObject(anyobject){
+   console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+handleObject(user)
+handleObject({
+    name: "isha",
+    price: 899
+})//we also write the object in this way
+
+// pass the Array in the function 
+
+const myNewArr = [200, 300, 600, 798]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondValue(myNewArr))//same work we will give the array in the on calling the function 
+console.log(returnSecondValue([
+    220, 330, 660, 798
+]))
